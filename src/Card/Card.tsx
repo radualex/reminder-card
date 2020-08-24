82f9d11c005d54dd180410dc1998bc1d0fb2c830
Radu Stoica <radu.stoica1994@gmail.com>
2020-08-24 21:41:12 +0200

    added components

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -5,6 +5,20 @@ export default function Card() {
   return (
     <div className="card-wrapper">
       <span>Viewing</span>
+      <span>Wed</span>
+      <div className="card-date-wrapper">
+        <span className="date">August 26</span>
+        <span className="time">17:00 - 18:00</span>
+      </div>
+      <div className="card-add-to-calendar-wrapper">
+        <span className="material-icons">add</span>
+        <span className="text">Add to calendar</span>
+      </div>
+      <div className="note">
+        <span className="text">
+          Remember to read the complete sales statement before viewing.
+        </span>
+      </div>
     </div>
   );
 }
