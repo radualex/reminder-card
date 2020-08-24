b92ea5f59da37b738853130080f9a5b66519a98b
Radu Stoica <radu.stoica1994@gmail.com>
2020-08-24 21:27:42 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./styles.css";
+
+import Card from "./Card/Card";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
